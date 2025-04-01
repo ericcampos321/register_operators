6da0efa24a98a43884172b4cebe99d641239e2e0
Eric Campos <ericaguiar_@hotmail.com>
2025-04-01 01:34:13 -0300

    feature/table-operators

---
+++ src/components/ui/Navbar.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import Button from 'primevue/button';
+</script>
+
+
+<template>
+    <div>
+        <!-- Navbar fixo -->
+        <nav class="fixed top-0 left-0 right-0 flex items-center justify-between p-4 bg-white shadow-md z-50">
+            <div class="flex items-center gap-2">
+                <img src="../../assets/logo.png" alt="IntuitLive" width="180" />
+            </div>
+            <Button label="Sign out" unstyled
+                pt:root="bg-purple-600 border-black text-white px-4 py-2 rounded-md hover:bg-black transition-colors" />
+        </nav>
+
+        <!-- Sidebar fixa -->
+        <div class="fixed top-16 left-0 p-4 w-60 h-screen opacity-80 z-40 bg-purple-600 shadow-md">
+            <div class="w-full mt-4">
+                <ul class="w-full">
+                    <li class="mb-4 flex w-full">
+                        <Button unstyled
+                            class="w-full flex items-center justify-center gap-2 bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-black hover:text-purple-600 transition-colors">
+                            Dashboard
+                        </Button>
+                    </li>
+                    <li class="mb-4 flex w-full ">
+                        <Button unstyled
+                            class="w-full flex items-center justify-center gap-2 bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-black hover:text-purple-600 transition-colors">
+                            Operators
+                        </Button>
+                    </li>
+                    <li class="mb-4 flex w-full ">
+                        <Button unstyled
+                            class="w-full flex items-center justify-center gap-2 bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-black hover:text-purple-600 transition-colors">
+                            Admin
+                        </Button>
+                    </li>
+                    <li class="mb-4 flex w-full ">
+                        <Button unstyled
+                            class="w-full flex items-center justify-center gap-2 bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-black hover:text-purple-600 transition-colors">
+                            Settings
+                        </Button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+import './assets/app.css'
+
+const app = createApp(App)
+app.mount('#app')
+
